1a7aad7c9c08f1ec7ce64215d0defb19522007c9
Sam Allen <samallen659@gmail.com>
2024-03-04 15:51:18 +0000

    adds marshall method for getting response in bytes

--- internal/server/response.go
+++ internal/server/response.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 
 	"golang.org/x/exp/slices"
 )
@@ -24,6 +26,13 @@ type Response struct {
 	Body   string
 }
 
+func NewResponse() *Response {
+	return &Response{
+		ResponseHeader{},
+		"",
+	}
+}
+
 func (r *Response) SetStatus(status HTTPStatus) error {
 	if !slices.Contains(HTTPStatuses, status) {
 		return errors.New("Invalid HTTP Status")
@@ -39,4 +48,12 @@ func (r *Response) SetHeader(key string, value string) {
 
 func (r *Response) SetBody(body string) {
 	r.Body = body
+}
+
+func (r *Response) Marshall() []byte {
+	var b bytes.Buffer
+	b.Write([]byte(fmt.Sprintf("HTTP/1.1 %d OK\r\n\r\n", r.Header.Status)))
+	b.Write([]byte(r.Body))
+
+	return b.Bytes()
 }
