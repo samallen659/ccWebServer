762c1c9aab49162f63785a9fe249f92bb2df0b41
Sam Allen <samallen659@gmail.com>
2024-03-04 15:43:54 +0000

    adds response methods

--- internal/server/response.go
+++ internal/server/response.go
@@ -1,10 +1,18 @@
 package server
 
+import (
+	"errors"
+
+	"golang.org/x/exp/slices"
+)
+
 type HTTPStatus int
 
 const (
 	HTTP_OK HTTPStatus = 200
 )
+
+var HTTPStatuses = []HTTPStatus{HTTP_OK}
 
 type ResponseHeader struct {
 	Status     HTTPStatus
@@ -14,4 +22,21 @@ type ResponseHeader struct {
 type Response struct {
 	Header ResponseHeader
 	Body   string
+}
+
+func (r *Response) SetStatus(status HTTPStatus) error {
+	if !slices.Contains(HTTPStatuses, status) {
+		return errors.New("Invalid HTTP Status")
+	}
+
+	r.Header.Status = status
+	return nil
+}
+
+func (r *Response) SetHeader(key string, value string) {
+	r.Header.Additional[key] = value
+}
+
+func (r *Response) SetBody(body string) {
+	r.Body = body
 }
