508bf1aa486a51f5252ce49b4a42c32d88778eef
Sam Allen <samallen659@gmail.com>
2024-03-04 16:33:06 +0000

    updates main to handle router dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/samallen659/ccWebServer/internal/server"
 )
 
 func main() {
-	svr, err := server.NewServer("localhost:8080")
+	root, err := os.Getwd()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	wwwPath := fmt.Sprintf("%s/www", root)
+	svr, err := server.NewServer("localhost:8080", wwwPath)
 	if err != nil {
 		log.Panic(err)
 	}
